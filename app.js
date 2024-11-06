3661169dc2fa2fbc705cf58744249aa2b3d129d1
Katherine Yang <whykatherine@gmail.com>
2024-11-06 18:24:02 -0500

    rewrite some of the js

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
+let browseDocuments = [];
 
-let results;
+const loadingText = document.getElementById("loading");
+const searchSection = document.getElementById("search");
+const browseSection = document.getElementById("browse");
 
-var index = new FlexSearch.Document({
+const browseTab = document.getElementById("browse-tab");
+browseTab.addEventListener("click", () => activateBrowse());
+const searchTab = document.getElementById("search-tab");
+searchTab.addEventListener("click", () => activateSearch());
+
+const searchInput = document.getElementById('search-input');
+
+const POST_TEMPLATE = document.querySelector(".search_item").cloneNode(true);
+POST_TEMPLATE.remove();
+
+const pageSize = 50;
+let pageMax = 1;
+let page = 1;
+let browseIndex = (page - 1) * pageSize;
+
+const index = new FlexSearch.Document({
 	encode: function(str){
 		const cjkItems = str.replace(/[\x00-\x7F]/g, "").split("");
 		const asciiItems = str.toLowerCase().split(/\W+/);
@@ -14,11 +32,8 @@ var index = new FlexSearch.Document({
   }
 });
 
-
-const searchInput = document.getElementById('search-input');
-
 function processData(data) {
-  for (doc of data) {
+  for (const doc of data) {
     index.add({
         id_str: doc.id_str,
         created_at: doc.created_at,
@@ -27,103 +42,122 @@ function processData(data) {
         retweet_count: doc.retweet_count
     })
   };
-  document.getElementById('loading').hidden = true;
-  document.getElementById('search').hidden = false;
+  loadingText.hidden = true;
+  searchSection.hidden = false;
 }
 
-processData(searchDocuments);
-let browseDocuments = searchDocuments.sort(function(a,b){
-  return new Date(b.created_at) - new Date(a.created_at);
-});
+fetch("./documents.json")
+  .then(r => r.json())
+  .then(documents => {
+    processData(documents);
 
-function sortResults(criterion) {
-  if (criterion === 'newest-first') {
-    results = results.sort(function(a,b){
+    browseDocuments = documents.sort(function(a,b){
       return new Date(b.created_at) - new Date(a.created_at);
     });
-    renderResults();
-  }
-  if (criterion === 'oldest-first') {
-    results = results.sort(function(a,b){
-      return new Date(a.created_at) - new Date(b.created_at);
-    });
-    renderResults();
-  }
-  if (criterion === 'most-relevant') {
-    results = results.sort(function(a,b){
-      return a.index - b.index;
-    });
-    renderResults();
-  }
-  if (criterion === 'most-popular') {
-    results = results.sort(function(a,b){
-      return (+b.favorite_count + +b.retweet_count) - (+a.favorite_count + +a.retweet_count);
-    });
-    renderResults();
-  }
-  if (criterion === 'newest-first-browse') {
-    browseDocuments = browseDocuments.sort(function(a,b){
-      return new Date(b.created_at) - new Date(a.created_at);
-    });
+
+    setPaging();
+
     renderBrowse();
-  }
-  if (criterion === 'oldest-first-browse') {
-    browseDocuments = browseDocuments.sort(function(a,b){
-      return new Date(a.created_at) - new Date(b.created_at);
-    });
-    renderBrowse();
-  }
-  if (criterion === 'most-popular-browse') {
-    browseDocuments = browseDocuments.sort(function(a,b){
-      return (+b.favorite_count + +b.retweet_count) - (+a.favorite_count + +a.retweet_count);
-    });
-    renderBrowse();
-  }
+  });
+
+function setPaging() {
+  pageMax = Math.floor(browseDocuments.length / pageSize) + 1;
+  document.getElementById('page-total').innerText = pageMax;
+  document.getElementById('page-num').addEventListener('input', onPageNumChange);
+  document.getElementById('page-num').value = +page;
+  document.getElementById('page-num').max = pageMax;
+  document.getElementById('page-num').min = 1;
 }
 
-function renderResults() {
-  const output = results.map(item => `<p class="search_item"><div class="search_link"><a href="kayserifserif/status/${item.id_str}">link</a></div> <div class="search_text">${item.full_text}</div><div class="search_time">${new Date(item.created_at).toLocaleString()}</div><hr class="search_divider" /></p>`.replace(/\.\.\/\.\.\/tweets_media\//g,'kayserifserif/tweets_media/'));
-  document.getElementById('output').innerHTML = output.join('');
+// function sortResults(criterion) {
+//   if (criterion === 'newest-first') {
+//     results = results.sort(function(a,b){
+//       return new Date(b.created_at) - new Date(a.created_at);
+//     });
+//     renderResults();
+//   }
+//   if (criterion === 'oldest-first') {
+//     results = results.sort(function(a,b){
+//       return new Date(a.created_at) - new Date(b.created_at);
+//     });
+//     renderResults();
+//   }
+//   if (criterion === 'most-relevant') {
+//     results = results.sort(function(a,b){
+//       return a.index - b.index;
+//     });
+//     renderResults();
+//   }
+//   if (criterion === 'most-popular') {
+//     results = results.sort(function(a,b){
+//       return (+b.favorite_count + +b.retweet_count) - (+a.favorite_count + +a.retweet_count);
+//     });
+//     renderResults();
+//   }
+//   if (criterion === 'newest-first-browse') {
+//     browseDocuments = browseDocuments.sort(function(a,b){
+//       return new Date(b.created_at) - new Date(a.created_at);
+//     });
+//     renderBrowse();
+//   }
+//   if (criterion === 'oldest-first-browse') {
+//     browseDocuments = browseDocuments.sort(function(a,b){
+//       return new Date(a.created_at) - new Date(b.created_at);
+//     });
+//     renderBrowse();
+//   }
+//   if (criterion === 'most-popular-browse') {
+//     browseDocuments = browseDocuments.sort(function(a,b){
+//       return (+b.favorite_count + +b.retweet_count) - (+a.favorite_count + +a.retweet_count);
+//     });
+//     renderBrowse();
+//   }
+// }
+
+function renderResults(results) {
+  const output = document.getElementById("output");
+  output.innerHTML = "";
+
+  results.forEach(item => {
+    const element = POST_TEMPLATE.cloneNode(true);
+    output.appendChild(element);
+
+    element.querySelector(".search_link a").href = `kayserifserif/status/${item.id_str}`;
+    // element.querySelector(".search_text").textContent = item.full_text;
+    element.querySelector(".search_text").innerHTML = item.full_text;
+    element.querySelector(".search_time").textContent = (new Date(item.created_at)).toLocaleString();
+  });
+
   if (results.length > 0) {
-    document.getElementById('output').innerHTML += '<a href="#tabs">top &uarr;</a>';
+    output.innerHTML += '<a href="#tabs">top &uarr;</a>';
   }
 }
 
 function onSearchChange(e) {
-  results = index.search(e.target.value, { enrich: true });
+  let results = index.search(e.target.value, { enrich: true });
   if (results.length > 0) {
     // limit search results to the top 100 by relevance
     results = results.slice(0,100);
     // preserve original search result order in the 'index' variable since that is ordered by relevance
     results = results[0].result.map((item, index) => { let result = item.doc; result.index = index; return result;});
   }
-  renderResults();
+  renderResults(results);
 }
 searchInput.addEventListener('input', onSearchChange);
 
-function searchTab() {
-  const clickedTab = document.getElementById('search-tab');
-  clickedTab.classList.add('active');
-  const otherTab = document.getElementById('browse-tab');
-  otherTab.classList.remove('active');
-  document.getElementById('browse').hidden = true;
-  document.getElementById('search').hidden = false;
+function activateSearch() {
+  searchTab.classList.add('active');
+  browseTab.classList.remove('active');
+  browseSection.hidden = true;
+  searchSection.hidden = false;
 }
 
-function browseTab() {
-  const clickedTab = document.getElementById('browse-tab');
-  clickedTab.classList.add('active');
-  const otherTab = document.getElementById('search-tab');
-  otherTab.classList.remove('active');
-  const searchContent = document.getElementById('search');
-  document.getElementById('search').hidden = true;
-  document.getElementById('browse').hidden = false;
+function activateBrowse() {
+  browseTab.classList.add('active');
+  searchTab.classList.remove('active');
+  searchSection.hidden = true;
+  browseSection.hidden = false;
 }
-
-const pageSize = 50;
-const pageMax = Math.floor(browseDocuments.length/pageSize) + 1;
-let page = 1;
-let browseIndex = (page - 1) * pageSize;
 
 function onPageNumChange(e) {
   page = e.target.value;
@@ -131,16 +165,20 @@ function onPageNumChange(e) {
   renderBrowse();
 }
 
-document.getElementById('page-total').innerText = pageMax;
-document.getElementById('page-num').addEventListener('input', onPageNumChange);
-document.getElementById('page-num').value = +page;
-document.getElementById('page-num').max = pageMax;
-document.getElementById('page-num').min = 1;
-
 function renderBrowse() {
-  const output = browseDocuments.slice(browseIndex, browseIndex + pageSize).map(item => `<p class="search_item"><div class="search_link"><a href="kayserifserif/status/${item.id_str}">link</a></div> <div class="search_text">${item.full_text}</div><div class="search_time">${new Date(item.created_at).toLocaleString()}</div><hr class="search_divider" /></p>`.replace(/\.\.\/\.\.\/tweets_media\//g,'kayserifserif/tweets_media/'));
-  document.getElementById('browse-output').innerHTML = output.join('');
-  document.getElementById('browse-output').innerHTML += '<a href="#tabs">top &uarr;</a>';
-}
+  const output = document.getElementById("browse-output");
+  output.innerHTML = "";
 
-renderBrowse();
+  const pageItems = browseDocuments.slice(browseIndex, browseIndex + pageSize);
+  pageItems.forEach(item => {
+    const element = POST_TEMPLATE.cloneNode(true);
+    output.appendChild(element);
+
+    element.querySelector(".search_link a").href = `kayserifserif/status/${item.id_str}`;
+    // element.querySelector(".search_text").textContent = item.full_text;
+    element.querySelector(".search_text").innerHTML = item.full_text;
+    element.querySelector(".search_time").textContent = (new Date(item.created_at)).toLocaleString();
+  })
+
+  output.innerHTML += '<a href="#tabs">top &uarr;</a>';
+}
